b031926fcea2b56d4e0b228e2ec7a12d561528cf
Ilia <iliageek1@gmail.com>
2024-12-22 11:55:41 -0800

    Lesson 1 Added

---
+++ Lesson 1/src/Main.java
@@ -0,0 +1,41 @@
+// With "//" We Can Comment in Java!
+
+// First We Should Make a Class    // public is an Access Modifier
+public class Main { // Start of class
+
+    // Body of class
+
+    // Now I Make a Method in Main Class With That Function Arguments    // static void is a Keyword
+    public static void main(String[] args) { // Start of Method
+
+        // Body of Method
+
+        // Print(Show) Hello,World! in Output
+        System.out.print("Hello, World!");
+
+        // Make a Variable; First That Data Type (String) Then The Name of Variable & in The End The Text That You Want To Show in Output
+        String message = "Hello Java";
+
+        // Here We Call That Variable(message). It's a Statement.
+        System.out.println(message);
+
+    } // End of Method
+} // End of class
+
+
+
+// For Making a Class in Java:
+// 1 - First We Should Determine The Access Modifier
+// 2 - Second We Should put Class Keyword
+// 3 - Then We Should Type Name of Class(Pascal Case)
+// 4 - Type Curly Brackets or Braces
+
+
+// For Making a Method in Java:
+// * Method Should Be in The Body of Class
+// 1 - First We Should Type Access Modifier
+// 2 - Put Static Keyword
+// 3 - Then We Type, Type of Output
+// 4 - Type The Name of Method
+// 5 - Then We Put Method's Arguments
+// 6 - And in The End We put Curly Brackets
